9311a2f63190bd66802bf2a405e692fcd04f72e3
Lucas de Jesus <lucaskreutzerdejesus@gmail.com>
2022-08-08 18:17:09 -0300

    Update maquina_estados.py

--- maquina_estados.py
+++ maquina_estados.py
@@ -21,64 +21,59 @@ A  saída  poderá  ser  enviada  para  um  arquivo  de  textos,  ou  para  o  t
 linha de saída por string de entrada. No caso do exemplo, teremos 3 linhas de saída.
 """
 
-# Importando biblioteca responsável pelo acesso aos diretórios.
-import os
-
-
 # Definição de uma função para lidar com a validação das palavras.
 def check_string(palavra):
-    resultado = False                   # Inicializando uma variável de retorno.
-    palavra_min = palavra.lower()       # Deixando a palavra em minúsculo para facilitar a validação.
-    letras_possiveis = ['a', 'b', 'c']  # Lista com os caracteres possíveis, de acordo com o alfabeto proposto.
-    tamanho_palavra = len(palavra)      # Variável para armazenar o tamanho da palavra.
+  resultado = False                   # Inicializando uma variável de retorno.
+  palavra_min = palavra.lower()       # Deixando a palavra em minúsculo para facilitar a validação.
+  letras_possiveis = ['a', 'b', 'c']  # Lista com os caracteres possíveis, de acordo com o alfabeto proposto.
+  tamanho_palavra = len(palavra)      # Variável para armazenar o tamanho da palavra.
 
-    # Loop para checar cada letra dentro da palavra em questão.
-    for i in range(0, tamanho_palavra):
-        letra = palavra_min[i]
+  # Loop para checar cada letra dentro da palavra em questão.
+  for i in range(0, tamanho_palavra):
+    letra = palavra_min[i]
 
-        # Caso seja um caracter inválido, retornará falso.
-        if letra not in letras_possiveis:
-            return False
-        elif letra == 'a':
-            # Caso o caracter 'a' esteja na última ou penúltima posição, sabemos que a palavra está inválida.
-            # Essa condição vem antes para evitar erro de "Out of Bounds".
-            if i == tamanho_palavra-1 or i == tamanho_palavra-2:
-                return False
-            # Caso contrário, checaremos os dois caracteres posteriores.
-            if palavra_min[i+1] == 'b' and palavra_min[i+2] == 'b':
-                resultado = True
-            else:
-                return False
+    # Caso seja um caracter inválido, retornará falso.
+    if letra not in letras_possiveis:
+      return False
+    elif letra == 'a':
+      # Caso o caracter 'a' esteja na última ou penúltima posição, sabemos que a palavra está inválida.
+      # Essa condição vem antes para evitar erro de "Out of Bounds".
+      if i == tamanho_palavra-1 or i == tamanho_palavra-2:
+        return False
+      # Caso contrário, checaremos os dois caracteres posteriores.
+      if palavra_min[i+1] == 'b' and palavra_min[i+2] == 'b':
+        resultado = True
+      else:
+        return False
 
-    return resultado
+  return resultado
 
 
-# Variável que irá armazenar os diretórios dentro da pasta em questão.
-# Obs: o programa só irá ler o arquivo se ele for inserido dentro do caminho passado no parâmetro.
-# Portanto, peço que insira seu arquivo nessa pasta, ou altere o caminho.
-arquivos = os.listdir('Arquivos')
+# Variável que irá armazenar o nome do arquivo que será lido.
+# Portanto, para realizar os testes, é necessário alterar o valor aqui.
+arquivo = "strings3.txt"
 
-# Loop para passar por cada arquivo, seguido por um comando que retornará a extensão do arquivo, para validação.
-for arquivo in arquivos:
-    extensao = os.path.splitext(arquivo)[1]
-    if extensao == ".txt":
-        # Caso o arquivo seja do tipo '.txt', ele será aberto e lido.
-        # A estrutura 'with' efetua o fechamento automático do arquivo após seu uso.
-        with open("Arquivos/%s" % arquivo) as dados:
-            nomes = dados.readlines()
-            qtd_palavras = int(nomes[0])
+# Importante notar que eu direcionei a abertura de arquivos dentro da pasta 'Arquivos'. 
+# Então, peço que mova qualquer arquivo novo desejado para a pasta indicada, ou altere o caminho no parâmetro abaixo.
+# A estrutura 'with' efetua o fechamento automático do arquivo após seu uso.
+with open("Arquivos/%s" % arquivo) as dados:
+  nomes = dados.readlines()
+  qtd_palavras = int(nomes[0])
 
-            contador = 1
-            print("\nArquivo --> %s:" % arquivo)
-            # Loop para checar cada palavra dentro do arquivo.
-            while contador <= qtd_palavras:
-                # Retirando o caracter adicional '\n' e chamando a função de checagem.
-                palavra = nomes[contador].strip('\n')
-                resposta = check_string(palavra)
-
-                # Realizando o print do resultado de acordo com o retorno da função.
-                if resposta:
-                    print("%s: pertence." % palavra)
-                else:
-                    print("%s: não pertence." % palavra)
-                contador += 1
+  # Checando se o inteiro passado na primeira linha do arquivo bate com a quantidade de palavras existentes nas linhas seguintes.
+  if len(nomes) == qtd_palavras+1:
+    contador = 1
+    # Loop para checar cada palavra dentro do arquivo.
+    while contador <= qtd_palavras:
+      # Retirando o caracter adicional '\n' e chamando a função de checagem.
+      palavra = nomes[contador].strip('\n')
+      resposta = check_string(palavra)
+  
+      # Realizando o print do resultado de acordo com o retorno da função.
+      if resposta:
+        print("%s: pertence." % palavra)
+      else:
+        print("%s: não pertence." % palavra)
+      contador += 1
+  else:
+    print("Quantidade de palavras não corresponde.")
